2e372dd5f0c622ee8b6765eb19b9b2dad20dfed0
monroenikko <monroenikkoaldrich@yahoo.com>
2019-05-29 22:55:57 +0800

    updates

---
+++ build/precache-manifest.1d04ae144d7e30f00066a4114befcf8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e040281fa2c14dcd65540fbabeb2ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02b91bb82ba82a05aae1",
+    "url": "/static/css/main.d6fa7445.chunk.css"
+  },
+  {
+    "revision": "98e10202d7a079230bf3",
+    "url": "/static/js/2.3cb41707.chunk.js"
+  },
+  {
+    "revision": "02b91bb82ba82a05aae1",
+    "url": "/static/js/main.5da32cb8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import './resources/styles.css';
+import Header from './components/header_footer/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
