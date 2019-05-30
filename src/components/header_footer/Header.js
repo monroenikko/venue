6e45b9de02cc4ebdae0e87e90e97c4a286d1ceb3
monroenikko <monroenikkoaldrich@yahoo.com>
2019-05-31 07:49:06 +0800

    update

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -9,7 +9,28 @@ import SideDrawer from './SideDrawer';
 class Header extends Component {
 
     state ={
-        drawerOpen: false
+        drawerOpen: false,
+        headerShow: false
+    }
+
+    componentDidMount()
+    {
+        window.addEventListener('scroll', this.handleScroll)
+    }
+
+    handleScroll = () => {
+        if(window.scrollY > 0)
+        {
+            this.setState({
+                headerShow: true
+            })
+        }
+        else
+        {
+            this.setState({
+                headerShow: false
+            })
+        }
     }
 
     toggleDrawer = (value) => {
@@ -24,10 +45,9 @@ class Header extends Component {
                 <AppBar
                     position="fixed"
                     style={{ 
-                            backgroundColor:'#2f2f2f',
+                            backgroundColor: this.state.headerShow ? '#2f2f2f' : 'transparent',
                             boxShadow: 'none',
-                            padding: '10px 0px',
-                            
+                            padding: '10px 0px',                            
                      }}                    
                 > 
                     <Toolbar>
@@ -41,6 +61,7 @@ class Header extends Component {
                         aria-label="Menu"
                         color="inherit"
                         onClick={()=> this.toggleDrawer(true)}
+                        
                     >
                         <MenuIcon/>
                     </IconButton>
